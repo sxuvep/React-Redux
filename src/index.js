11a5157f0969f0fdd568b88c12fd10f91ad2080b
vamsee <pasangulapativamsee@gmail.com>
2018-02-02 20:16:52 -0500

    Added delete post

--- src/index.js
+++ src/index.js
@@ -2,27 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import PostsIndex from './components/post_index';
 import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
 
 import reducers from './reducers';
 import reduxPromise from 'redux-promise';
 
 const createStoreWithMiddleware = applyMiddleware(reduxPromise)(createStore);
- 
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-    <div>
-      <Switch>
-        <Route path="/posts/new" component={PostsNew}/>
-        <Route path="/" component={PostsIndex}/>
-      </Switch>
-     
-    </div>
-    </BrowserRouter>
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<BrowserRouter>
+			<div>
+				<Switch>
+					<Route path="/posts/new" component={PostsNew} />
+					<Route path="/posts/:id" component={PostsShow} />
+					<Route path="/" component={PostsIndex} />
+				</Switch>
+			</div>
+		</BrowserRouter>
+	</Provider>,
+	document.querySelector('.container')
+);
